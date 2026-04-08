440e5358e98163587f4cdb8537c2091a7006624a
Andbal23 <laszlo2002andras@gmail.com>
2026-04-09 01:32:08 +0200

    v3

--- extension.js
+++ extension.js
@@ -1,8 +1,6 @@
 import Gio from 'gi://Gio';
 import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
-import St from 'gi://St';
-import Clutter from 'gi://Clutter';
 
 import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
@@ -10,168 +8,201 @@ import { QuickMenuToggle } from 'resource:///org/gnome/shell/ui/quickSettings.js
 import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 const DndTimerToggle = GObject.registerClass(
-class DndTimerToggle extends QuickMenuToggle {
-    _init(extension) {
-        super._init({
-            title: _('Do Not Disturb'),
-            iconName: 'notifications-disabled-symbolic',
-            toggleMode: true
-        });
-
-        this._ext = extension;
-        this._settings = new Gio.Settings({ schema_id: 'org.gnome.desktop.notifications' });
-
-        this._buildMenu();
-        
-        this._settingsId = this._settings.connect('changed::show-banners', () => this._syncState());
-        this.connect('clicked', () => this._toggleDnd());
-
-        this._syncState();
-    }
-
-    _buildMenu() {
-        const presets = [
-            { label: _('15m'), mins: 15 },
-            { label: _('30m'), mins: 30 },
-            { label: _('1h'), mins: 60 },
-            { label: _('2h'), mins: 120 },
-            { label: _('4h'), mins: 240 },
-            { label: _('8h'), mins: 480 }
-        ];
-
-        const gridItem = new PopupMenu.PopupBaseMenuItem({ reactive: false });
-        gridItem.set_style('padding: 8px 12px;'); 
-
-        const gridVBox = new St.BoxLayout({
-            vertical: true,
-            x_expand: true,
-            style_class: 'dnd-preset-grid'
-        });
-
-        const row1 = new St.BoxLayout({ vertical: false, x_expand: true, style_class: 'dnd-preset-row' });
-        const row2 = new St.BoxLayout({ vertical: false, x_expand: true, style_class: 'dnd-preset-row' });
-
-        for (let i = 0; i < presets.length; i++) {
-            const preset = presets[i];
-            const btn = new St.Button({
-                label: preset.label,
-                style_class: 'dnd-preset-btn',
-                x_expand: true,
-                x_align: Clutter.ActorAlign.FILL,
-                can_focus: true
-            });
-            
-            btn.connect('clicked', () => {
-                this._ext.startTimer(preset.mins);
-                this.menu.close();
+    class DndTimerToggle extends QuickMenuToggle {
+        _init(extension) {
+            super._init({
+                title: _('Do Not Disturb'),
+                iconName: 'notifications-disabled-symbolic',
+                toggleMode: true,
             });
 
-            if (i < 3) {
-                row1.add_child(btn);
+            this._ext = extension;
+            this._settings = new Gio.Settings({ schema_id: 'org.gnome.desktop.notifications' });
+            this._extSettings = extension.getSettings();
+            this._presetItems = new Map();
+
+            this._buildMenu();
+
+            this._settingsId = this._settings.connect(
+                'changed::show-banners', () => this._syncState()
+            );
+            this._extSubtitleId = this._extSettings.connect(
+                'changed::show-timer-subtitle', () => this._syncState()
+            );
+            this._extDurationsId = this._extSettings.connect(
+                'changed::timer-durations', () => this._buildMenu()
+            );
+            this.connect('clicked', () => this._toggleDnd());
+
+            this._syncState();
+        }
+
+        _formatDuration(minutes) {
+            if (minutes < 60) return `${minutes} ${_('minutes')}`;
+
+            const hours = Math.floor(minutes / 60);
+            const mins = minutes % 60;
+
+            let str = `${hours} ${_('hour')}${hours > 1 ? 's' : ''}`;
+            if (mins > 0) str += ` ${mins} ${_('min')}`;
+            return str;
+        }
+
+        _buildMenu() {
+            if (!this._itemsSection) {
+                this.menu.setHeader(
+                    'notifications-disabled-symbolic',
+                    _('Do Not Disturb')
+                );
+
+                this._itemsSection = new PopupMenu.PopupMenuSection();
+                this.menu.addMenuItem(this._itemsSection);
+
+                this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+                const settingsItem = this.menu.addAction(_('Settings'), () => {
+                    this._ext.openPreferences();
+                });
+                settingsItem.visible = Main.sessionMode.allowSettings;
+            }
+
+            this._itemsSection.removeAll();
+            this._presetItems.clear();
+
+            const durations = this._extSettings.get_value('timer-durations').deepUnpack();
+
+            for (const mins of durations) {
+                if (mins <= 0) continue;
+
+                const item = new PopupMenu.PopupImageMenuItem(
+                    this._formatDuration(mins), 'alarm-symbolic'
+                );
+                item.connect('activate', () => {
+                    this._ext.startTimer(mins);
+                });
+                this._itemsSection.addMenuItem(item);
+                this._presetItems.set(mins, item);
+            }
+
+            const infiniteItem = new PopupMenu.PopupImageMenuItem(
+                _('Until I turn it off'), 'notifications-disabled-symbolic'
+            );
+            infiniteItem.connect('activate', () => {
+                this._ext._activeTimerMins = 0;
+                this._ext.cancelTimer();
+                this._settings.set_boolean('show-banners', false);
+                this._syncState();
+
+                if (this._extSettings.get_boolean('show-toggle-notification')) {
+                    Main.notify(
+                        _('Do Not Disturb'),
+                        _('Enabled')
+                    );
+                }
+            });
+            this._itemsSection.addMenuItem(infiniteItem);
+            this._presetItems.set(0, infiniteItem);
+
+            this._syncOrnaments();
+        }
+
+        _syncState() {
+            const isDndOn = !this._settings.get_boolean('show-banners');
+            this.checked = isDndOn;
+
+            if (!isDndOn) {
+                this.subtitle = null;
+                this._ext._activeTimerMins = -1;
+                this._ext.cancelTimer();
             } else {
-                row2.add_child(btn);
+                const showSubtitle = this._extSettings.get_boolean('show-timer-subtitle');
+                if (showSubtitle) {
+                    const endTime = this._ext.getEndTimeString();
+                    this.subtitle = endTime ? `${_('Until')} ${endTime}` : _('On');
+                } else {
+                    this.subtitle = _('On');
+                }
+            }
+
+            this._syncOrnaments();
+        }
+
+        _syncOrnaments() {
+            const isDndOn = !this._settings.get_boolean('show-banners');
+            const activeMins = this._ext.getActiveTimerMins();
+
+            for (const [mins, item] of this._presetItems) {
+                if (isDndOn && mins === activeMins) {
+                    item.setOrnament(PopupMenu.Ornament.CHECK);
+                } else {
+                    item.setOrnament(PopupMenu.Ornament.NONE);
+                }
             }
         }
 
-        gridVBox.add_child(row1);
-        gridVBox.add_child(row2);
-        gridItem.add_child(gridVBox);
-        this.menu.addMenuItem(gridItem);
+        _toggleDnd() {
+            const wasDndOn = !this._settings.get_boolean('show-banners');
 
-        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+            if (wasDndOn) {
+                this._ext._activeTimerMins = -1;
+                this._ext.cancelTimer();
+                this._settings.set_boolean('show-banners', true);
 
-        const customItem = new PopupMenu.PopupBaseMenuItem({ reactive: false });
-        customItem.set_style('padding: 8px 12px;');
+                if (this._extSettings.get_boolean('show-toggle-notification')) {
+                    Main.notify(
+                        _('Do Not Disturb'),
+                        _('Disabled')
+                    );
+                }
+            } else {
+                this._ext._activeTimerMins = 0;
+                this._settings.set_boolean('show-banners', false);
 
-        const box = new St.BoxLayout({ 
-            style_class: 'dnd-timer-custom-box', 
-            vertical: false, 
-            x_expand: true 
-        });
-
-        const clockIcon = new St.Icon({
-            icon_name: 'alarm-symbolic',
-            style_class: 'popup-menu-icon',
-            y_align: Clutter.ActorAlign.CENTER
-        });
-        clockIcon.set_style('margin-right: 8px;');
-
-        this._entry = new St.Entry({
-            hint_text: _('Custom (mins)...'),
-            x_expand: true,
-            can_focus: true,
-            style_class: 'dnd-timer-entry'
-        });
-
-        const startBtn = new St.Button({
-            label: _('Start'),
-            style_class: 'dnd-timer-start-btn',
-            can_focus: true,
-            y_align: Clutter.ActorAlign.CENTER
-        });
-
-        const onCustomStart = () => {
-            const val = parseInt(this._entry.get_text(), 10);
-            if (!isNaN(val) && val > 0) {
-                this._ext.startTimer(val);
-                this._entry.set_text('');
-                this.menu.close();
+                if (this._extSettings.get_boolean('show-toggle-notification')) {
+                    Main.notify(
+                        _('Do Not Disturb'),
+                        _('Enabled')
+                    );
+                }
             }
-        };
-
-        startBtn.connect('clicked', onCustomStart);
-        this._entry.clutter_text.connect('activate', onCustomStart);
-
-        box.add_child(clockIcon);
-        box.add_child(this._entry);
-        box.add_child(startBtn);
-        customItem.add_child(box);
-        
-        this.menu.addMenuItem(customItem);
-    }
-
-    _syncState() {
-        const isDndOn = !this._settings.get_boolean('show-banners');
-        this.checked = isDndOn;
-
-        if (!isDndOn) {
-            this.subtitle = null;
-            this._ext.cancelTimer();
-        } else {
-            const endTime = this._ext.getEndTimeString();
-            // Leteremtjük az 'Until' szót, hogy ezt is lehessen fordítani (pl. 'Eddig: 14:30')
-            this.subtitle = endTime ? `${_('Until')} ${endTime}` : _('On');
         }
-    }
 
-    _toggleDnd() {
-        const isDndOn = !this._settings.get_boolean('show-banners');
-        this._settings.set_boolean('show-banners', isDndOn);
-        
-        if (!isDndOn) {
-            this._ext.cancelTimer();
+        destroy() {
+            if (this._settingsId) {
+                this._settings.disconnect(this._settingsId);
+                this._settingsId = null;
+            }
+            if (this._extSubtitleId) {
+                this._extSettings.disconnect(this._extSubtitleId);
+                this._extSubtitleId = null;
+            }
+            if (this._extDurationsId) {
+                this._extSettings.disconnect(this._extDurationsId);
+                this._extDurationsId = null;
+            }
+            this._settings = null;
+            this._extSettings = null;
+            super.destroy();
         }
-    }
-
-    destroy() {
-        if (this._settingsId) {
-            this._settings.disconnect(this._settingsId);
-            this._settingsId = null;
-        }
-        this._settings = null;
-        super.destroy();
-    }
-});
+    });
 
 export default class DndTimerExtension extends Extension {
     enable() {
         this._timerId = null;
         this._endTimeString = null;
+        this._activeTimerMins = -1;
         this._settings = new Gio.Settings({ schema_id: 'org.gnome.desktop.notifications' });
+        this._extSettings = this.getSettings();
+
+        if (this._extSettings.get_boolean('remember-state')) {
+            const wasDndOn = !this._settings.get_boolean('show-banners');
+            if (wasDndOn) {
+                this._activeTimerMins = 0;
+            }
+        }
 
         const qsMenu = Main.panel.statusArea.quickSettings.menu;
         this._originalToggle = null;
-        
+
         for (const child of qsMenu._grid.get_children()) {
             if (child.constructor.name === 'DoNotDisturbToggle') {
                 this._originalToggle = child;
@@ -179,15 +210,11 @@ export default class DndTimerExtension extends Extension {
             }
         }
 
-        const originalTitle = this._originalToggle ? this._originalToggle.title : _('Do Not Disturb');
+        this._toggle = new DndTimerToggle(this);
 
-        this._toggle = new DndTimerToggle(this, originalTitle);
-        
         if (this._originalToggle) {
             this._originalToggle.hide();
-            
             qsMenu.addItem(this._toggle);
-            
             qsMenu._grid.set_child_below_sibling(this._toggle, this._originalToggle);
         } else {
             qsMenu.addItem(this._toggle);
@@ -195,8 +222,15 @@ export default class DndTimerExtension extends Extension {
     }
 
     disable() {
+        if (!this._extSettings.get_boolean('remember-state')) {
+            const isDndOn = !this._settings.get_boolean('show-banners');
+            if (isDndOn && this._timerId) {
+                this._settings.set_boolean('show-banners', true);
+            }
+        }
+
         this.cancelTimer();
-        
+
         if (this._toggle) {
             this._toggle.destroy();
             this._toggle = null;
@@ -206,28 +240,50 @@ export default class DndTimerExtension extends Extension {
             this._originalToggle.show();
             this._originalToggle = null;
         }
-        
+
         this._settings = null;
+        this._extSettings = null;
     }
 
     startTimer(minutes) {
         this.cancelTimer();
-        
+
+        this._activeTimerMins = minutes;
         this._settings.set_boolean('show-banners', false);
 
         const now = new Date();
         const endTime = new Date(now.getTime() + minutes * 60000);
-        this._endTimeString = endTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-        this._timerId = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, minutes * 60, () => {
-            this._timerId = null;
-            this._endTimeString = null;
-            
-            this._settings.set_boolean('show-banners', true);
-            Main.notify(_('Do Not Disturb Ended'), _('Notifications are enabled again.'));
-            
-            return GLib.SOURCE_REMOVE;
+        this._endTimeString = endTime.toLocaleTimeString([], {
+            hour: '2-digit', minute: '2-digit',
         });
+
+        if (this._extSettings.get_boolean('show-toggle-notification')) {
+            Main.notify(
+                _('Do Not Disturb'),
+                `${_('Until')} ${this._endTimeString}`
+            );
+        }
+
+        this._timerId = GLib.timeout_add_seconds(
+            GLib.PRIORITY_DEFAULT,
+            minutes * 60,
+            () => {
+                this._timerId = null;
+                this._endTimeString = null;
+                this._activeTimerMins = -1;
+
+                this._settings.set_boolean('show-banners', true);
+
+                if (this._extSettings.get_boolean('show-end-notification')) {
+                    Main.notify(
+                        _('Do Not Disturb Ended'),
+                        _('Notifications are enabled again.')
+                    );
+                }
+
+                return GLib.SOURCE_REMOVE;
+            }
+        );
 
         if (this._toggle) {
             this._toggle._syncState();
@@ -244,5 +300,9 @@ export default class DndTimerExtension extends Extension {
 
     getEndTimeString() {
         return this._endTimeString;
+    }
+
+    getActiveTimerMins() {
+        return this._activeTimerMins;
     }
 }
